86b3284616b5ea6ca912ce8854983e4748b17a34
israel ramirez <israelramirez.am@gmail.com>
2025-08-15 12:40:21 -0300

    movido contante a nueva archivo

--- Memoria/src/App.jsx
+++ Memoria/src/App.jsx
@@ -1,204 +1,37 @@
 import React from 'react'
 import { useState } from 'react'
 import './App.css'
-import { v4 as uuidv4 } from "uuid"
 import { Fichas } from "./componentes/fichas"
+import { FICHAS } from './contants'
 
 function App() {
   const [datos, setDatos] = useState({
-    contador : 0,
+    contador: 0,
     nombre: [""]
 
   })
 
-  const [fichas, setFichas] = useState([{
-    id:uuidv4(),
-    voltear : false,
-    imagen: "300ds",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "625dsx",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "790",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "1250gs",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "cb500",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "duke",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "g310gs",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "monster",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "mt09",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "ninja",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "r1",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "tracer900",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "versys",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "xadv",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "300ds",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "625dsx",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "790",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "1250gs",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "cb500",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "duke",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "g310gs",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "monster",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "mt09",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "ninja",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "r1",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "tracer900",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "versys",
-    resuelto : false
-  },
-  {
-    id:uuidv4(),
-    voltear : false,
-    imagen: "xadv",
-    resuelto : false
-  }])
+  const [fichas, setFichas] = useState(FICHAS)
 
   const manejarClic = id => {
 
-    fichas.forEach( ficha => {
-      if (ficha.id === id && ficha.resuelto == false){
+    fichas.forEach(ficha => {
+      if (ficha.id === id && ficha.resuelto == false) {
         ficha.voltear = !ficha.voltear
       }
-      if (ficha.voltear===true && datos.contador<2){
+      if (ficha.voltear === true && datos.contador < 2) {
         datos.nombre.push(ficha.imagen)
-        datos.contador = contador++
+        datos.contador = datos.contador++
       }
     });
-    if (datos.contador == 2){
-      if (datos.nombre[0]==datos.nombre[1]){
+    if (datos.contador == 2) {
+      if (datos.nombre[0] == datos.nombre[1]) {
         const fichasActualizadas = fichas.map((ficha) => {
-          if (ficha.imagen == datos.nombre[0]){
-            ficha.resuelto ===true
+          if (ficha.imagen == datos.nombre[0]) {
+            ficha.resuelto === true
             setDatos({
-              contador : 0,
-              nombre : [""]
+              contador: 0,
+              nombre: [""]
             })
           }
         })
@@ -212,14 +45,14 @@ function App() {
     <>
       <div className='componente-principal'>
         {fichas.map((ficha) => (
-          <Fichas 
+          <Fichas
             key={ficha.id}
             id={ficha.id}
             voltear={ficha.voltear}
             imagen={ficha.imagen}
             resuelto={ficha.resuelto}
             manejarClic={manejarClic} />
-          ))
+        ))
         }
       </div>
     </>
